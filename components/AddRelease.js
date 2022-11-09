e6804777540b323f14a0bf684b089738d78d5c50
ismail <ismail@bazookka.com>
2022-11-09 10:14:51 -0500

    Implement adding album to database

---
+++ components/AddRelease.js
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+import { appendErrors, Controller, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+import { supabase } from "../supabaseClient";
+import { DatePicker } from "@mantine/dates";
+
+const schema = yup.object({
+  releaseDate: yup.string().required().min(2),
+  album: yup.string().required().min(2),
+  artist: yup.string().required().min(2),
+})
+
+export default function AddRelease({setInsertedData}) {
+
+  const { register, control, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema)
+  });
+  const [data, setData] = useState("");
+
+  const insertRelease = async (rel) => {
+    rel.releaseDate = new Date (rel.releaseDate).toISOString()
+    const { error, data } = await supabase.from("Releases").insert(rel).select('*')
+    console.log("inserted ", data)
+    console.log("error ",error)
+    if(data){
+      setInsertedData(data)
+    }
+    console.log("insert error ",error)
+  }
+
+  return (
+    <form onSubmit={handleSubmit((data) => insertRelease(data))}>
+      <div class="field">
+        <label class="label">Release Date</label>
+        <div class="control">
+          <Controller
+            name="releaseDate"
+            control={control}
+            render={({ field }) => <DatePicker placeholder="Pick date" label="Event date" withAsterisk {...field} dateParser={(dateString) => new Date(dateString).toISOString()} />}
+          />
+          <p>{errors.releaseDate?.message}</p>
+        </div>
+
+      </div>
+
+
+      <div class="field">
+        <label class="label">Artist</label>
+        <div class="control">
+          <input {...register("artist")} class="input" type="text" placeholder="Type an artist name" />
+          <p>{errors.artist?.message}</p>
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">Album</label>
+        <div class="control">
+          <input {...register("album")} class="input" type="text" placeholder="Type an album name" />
+          <p>{errors.album?.message}</p>
+        </div>
+      </div>
+
+
+      <div class="field is-grouped">
+        <div class="control">
+          <button class="button is-link">Submit</button>
+        </div>
+        <div class="control">
+          <button class="button is-link is-light">Cancel</button>
+        </div>
+      </div>
+    </form>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+import { useEffect, useState } from 'react'
+import AddRelease from '../components/AddRelease'
+import CollapsibleTable from '../components/CollapsibleTable'
+import { supabase } from '../supabaseClient';
 
 export const getStaticProps = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users")
@@ -12,7 +14,25 @@ export const getStaticProps = async () => {
 }
 
  const  Home = ({users}) => {
-  console.log("uuu ",users)
+  const [releases, setReleases] = useState([])
+  const [insertedData, setInsertedData] = useState([])
+
+
+  const getReleases = async () => {
+    const { data, error } = await supabase.from('Releases').select()
+    if(!error){
+      setReleases(data)
+    }
+    data && console.log("data ", data)
+
+  }
+
+  useEffect(() => {
+    getReleases()
+  }, [insertedData])
+
+
+  releases
   return (
     <>
     <Head>
@@ -20,16 +40,17 @@ export const getStaticProps = async () => {
       <meta name="keywords" content='release dates'/>
     </Head>
     <div 
-    // className={styles.container}
     >
       <h1>Homepage</h1>
-      { users.map(u => {
+      <CollapsibleTable data={releases}/>
+      <AddRelease setInsertedData={setInsertedData}/>
+      {/* { users.map(u => {
         return <div key={u.id}>
             <Link href={'users/'+ u.id}>
               <h3>{u.name}</h3>
             </Link>
         </div>
-      })}
+      })} */}
     </div>
     </>
   )
