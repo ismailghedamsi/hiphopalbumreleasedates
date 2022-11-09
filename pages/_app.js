16e91a42c30ef759800e77c5994bc4d9d9c139ae
ismail <ismail@bazookka.com>
2022-11-09 10:13:37 -0500

    Add Matine ui library

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
+import { MantineProvider } from '@mantine/core'
 import Layout from '../components/Layout'
 import '../styles/globals.css'
 
+
 function MyApp({ Component, pageProps }) {
   return (
+    <MantineProvider withGlobalStyles withNormalizeCSS>
     <Layout>
       <Component {...pageProps} />
     </Layout>
+    </MantineProvider>
   )
 }
 
