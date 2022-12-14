f8d35f651acf82ab074a8a6118e4ae7a89e9ff92
ismail <ismail@bazookka.com>
2022-12-14 16:46:18 -0500

    clean code

--- components/AddRelease.js
+++ components/AddRelease.js
@@ -4,14 +4,14 @@ import { yupResolver } from "@hookform/resolvers/yup"
 import * as yup from "yup"
 import { supabase } from "../supabaseClient";
 import { DatePicker } from "@mantine/dates";
-import { addMonths, getMonth, getYear } from "date-fns";
+import { getYear } from "date-fns";
 import dayjs from "dayjs";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import "../styles/AddRelease.module.css"
 import { Group, Image, SimpleGrid, Tabs, Text, TextInput, useMantineTheme } from "@mantine/core";
 import { Dropzone, IMAGE_MIME_TYPE } from "@mantine/dropzone";
-import { IconPhoto, IconUpload, IconX, IconHourglass } from "@tabler/icons";
+import { IconPhoto, IconUpload, IconX } from "@tabler/icons";
 import AppContext from "./AppContext";
 
 const schema = yup.object({
@@ -20,12 +20,11 @@ const schema = yup.object({
   artist: yup.string().required("You need to select the artist name").min(2),
 })
 
-export default function AddRelease({ setStartDate, setDefaultValueYearSelect, setYear, setMonth,month, setOpened, setInsertedData, setSelectedIndex, setSelectedYear }) {
+export default function AddRelease({ setStartDate, setDefaultValueYearSelect, setYear, setMonth,month, setInsertedData, setSelectedIndex, setSelectedYear }) {
   const [isUploading, setIsUploading] = useState(false)
   const [coverSource, setCoverSource] = useState("local")
   const [files, setFiles] = useState([]);
-  const { loggedUser, setLoggedUser } = useContext(AppContext)
-  console.log("logged user username ",loggedUser.id)
+  const { loggedUser } = useContext(AppContext)
 
   const theme = useMantineTheme();
 
