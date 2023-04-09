069544dd0ca6c5e4bdd64d8927c9b62c2463ec89
ismail <ismail@bazookka.com>
2023-04-09 09:23:52 -0400

    Improve ressources style

--- components/HipHopRessources.js
+++ components/HipHopRessources.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import styles from '../styles/Ressources.module.css';
 import { supabase } from '../supabaseClient';
 import { useQuery } from '@supabase-cache-helpers/postgrest-swr';
+import { Accordion } from '@mantine/core';
 
 const HipHopRessources = () => {
   const { data, error, isLoading } = useQuery(
@@ -33,29 +34,31 @@ const HipHopRessources = () => {
     }, {});
   }
   return (
-    <div>
+    <Accordion>
       {Object.entries(groupedResources).map(([category, subcategories]) => {
-        console.log("subcat ",Object.keys(subcategories).includes('null') )
-        return <div key={category}>
-          <h2 className={styles.category}>{category}</h2>
-          {Object.entries(subcategories).map(([subCategory, resources]) => (
-            <div key={`${category}-${subCategory}`} className={styles['subcategories-container']}>
-              {!Object.keys(subcategories).includes('null') && <h3 className={styles.subCategory}>{subCategory}</h3>}
-              <ul className={styles['resources-container']}>
-                {resources.map(resource => (
-                  <li key={resource.id} className={styles['resource-item']}>
-                    <a href={resource.url}>{resource.name || ''}</a>
-                    <p>{resource.description || ''}</p>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
+        console.log("subcat ", Object.keys(subcategories).includes('null'))
+        return <Accordion.Item value={category} key={category}>
+          <Accordion.Control className={styles.category}>{category}</Accordion.Control>
+          <Accordion.Panel>
+            {Object.entries(subcategories).map(([subCategory, resources]) => (
+              <div key={`${category}-${subCategory}`} className={styles['subcategories-container']}>
+                {!Object.keys(subcategories).includes('null') && <h3 className={styles.subCategory}>{subCategory}</h3>}
+                <ul className={styles['resources-container']}>
+                  {resources.map(resource => (
+                    <li key={resource.id} className={styles['resource-item']}>
+                      <a href={resource.url}>{resource.name || ''}</a>
+                      <p>{resource.description || ''}</p>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </Accordion.Panel>
+        </Accordion.Item>
       })}
       {error && <p>{error.message}</p>}
       {isLoading && <p>Loading...</p>}
-    </div>
+    </Accordion>
   );
 
 }
