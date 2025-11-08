be1a35106643c6e4c9610c95febf70f859279548
Ismail Ghedamsi <ismailghedamsi@gmail.com>
2025-11-08 17:01:14 -0500

    fix issues

--- components/DaysList.jsx
+++ components/DaysList.jsx
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react';
-import { Scrollbars } from 'react-custom-scrollbars';
 import AppContext from './AppContext';
 import { ListContainer, ListItem } from './styled/DaysList/DaysListStyles';
 
@@ -8,14 +7,12 @@ const DaysList = () => {
   const { setSelectedDayNumber, uniqueDays } = useContext(AppContext);
 
   return (
-    <Scrollbars autoHide>
-      <ListContainer>
-        <li>day</li>
-        {uniqueDays && uniqueDays.length > 0 && uniqueDays.map((el) => {
-          return <ListItem onClick={() => setSelectedDayNumber(el)}>{el}</ListItem>;
-        })}
-      </ListContainer>
-    </Scrollbars>
+    <ListContainer aria-label="Available release days">
+      <li>day</li>
+      {uniqueDays && uniqueDays.length > 0 && uniqueDays.map((el) => {
+        return <ListItem key={el} onClick={() => setSelectedDayNumber(el)}>{el}</ListItem>;
+      })}
+    </ListContainer>
   );
 };
 
